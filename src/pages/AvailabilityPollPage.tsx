a70e16ac199afe03666e6ad226bf45460d765729
Damian Grasso <damian_240z@protonmail.com>
2026-01-12 19:36:50 +1000

    Basic page setup

---
+++ src/pages/AvailabilityPollPage.tsx
@@ -0,0 +1,10 @@
+const AvailabilityPollPage = () => {
+
+    return (
+        <>
+            <h1>Availability Polls</h1>
+        </>
+    );
+};
+
+export default AvailabilityPollPage;
---
+++ src/pages/ResponsePage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ResponsePage = () => {
+
+    return (
+        <>
+            <h1>Response Page</h1>
+        </>
+    );
+};
+
+export default ResponsePage;
