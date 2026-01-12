5c7f3e418bb61de721c4faedaa9dbfdb63e02b10
damian-grasso <damian_240z@protonmail.com>
2026-01-13 08:27:30 +1000

    Setup base AddAvailabilityPollCardModal with react-hook-form

---
+++ src/components/AddAvailabilityPollCardModal.tsx
@@ -0,0 +1,48 @@
+import { useForm } from "react-hook-form";
+import { Button, Col, Form, Modal, Row } from "react-bootstrap";
+import type { AvailabilityPoll } from "../models/availability-poll";
+
+type AvailabilityPollCardProps = {
+    showModal: boolean,
+    handleClose: any,
+    chosenAvailabilityPoll: AvailabilityPoll | null,
+}
+
+const AddAvailabilityPollModal = ({ showModal, chosenAvailabilityPoll, handleClose }: AvailabilityPollCardProps) => {  
+
+    type AvailabilityPollFormInputs = {
+
+    };
+
+    const { register, handleSubmit, formState: { errors }, reset, watch } = useForm<AvailabilityPollFormInputs>();
+
+    const onSubmit = async () => {
+        
+    }
+
+    return (
+        <Modal show={showModal} 
+            key={chosenAvailabilityPoll ? chosenAvailabilityPoll.id : "new-fixture"}
+            onHide={() => {
+                reset();
+                handleClose(false);
+            }}>
+            <Modal.Header closeButton>
+                <Modal.Title>{ (chosenAvailabilityPoll == null) ? "Add Fixture" : "Update Fixture" }</Modal.Title>
+            </Modal.Header>
+            <Form onSubmit={ handleSubmit(onSubmit) }>
+                <Modal.Body>
+                    <Row>
+                        <Form.Group as={Col} xs={12} md={6} className="mb-4" />
+                    </Row>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="primary">Add / Update</Button>
+                    <Button variant="primary" onClick={ () => handleClose() }>Cancel</Button>
+                </Modal.Footer>
+            </Form>
+        </Modal>
+    );
+};
+
+export default AddAvailabilityPollModal;
--- src/pages/AvailabilityPollPage.tsx
+++ src/pages/AvailabilityPollPage.tsx
@@ -2,10 +2,18 @@ import { useEffect, useState } from "react";
 import { getAvailabilityPollsByAccount } from "../services/availability-poll-service";
 import type { AvailabilityPoll } from "../models/availability-poll";
 import AvailabilityPollCard from "../components/AvailabilityPollCard";
+import AddAvailabilityPollModal from "../components/AddAvailabilityPollCardModal";
 
 const AvailabilityPollPage = () => {
 
+    const [chosenAvailabilityPoll, setChosenAvailabilityPoll] = useState({} as AvailabilityPoll | null);
     const [availabilityPolls, setAvailabilityPolls] = useState(null as AvailabilityPoll[] | null);
+
+    const [showModal, setShowModal] = useState(true);
+
+    const handleClose = async () => {
+        setShowModal(false);
+    }
 
     const fetchAvailabilityPolls = async () => {
         const availabilityPolls = await getAvailabilityPollsByAccount();
@@ -22,8 +30,16 @@ const AvailabilityPollPage = () => {
 
     return (
         <>
+
             <h1>Availability Polls</h1>
             <hr/>
+            {
+                (showModal) &&
+                    <AddAvailabilityPollModal
+                        chosenAvailabilityPoll={chosenAvailabilityPoll}
+                        handleClose={handleClose}
+                        showModal={showModal} />
+            }
             <div>
             {
                 availabilityPolls?.map((poll: AvailabilityPoll) => {
